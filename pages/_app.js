788e2a41b14936c300c434f702b0ee03109b6051
Dacy <dacy@Dacys-MacBook-Pro.local>
2023-01-03 17:13:14 +0200

    implement a progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Router from "next/router";
 import ProgressBar from "@badrap/bar-of-progress";
 
 const progress = new ProgressBar({
-  size: 8,
+  size: 5,
   color: "#ff385c",
   className: "z-50",
   delay: 100,
